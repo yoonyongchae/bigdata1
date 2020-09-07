0db5a8e1ba4f2564be00a4bf46d4a7f350c32b76
SM000006 <SM000006@DESKTOP-2PR8TE2>
2020-09-07 16:44:44 +0900

    미션완료

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -3,10 +3,10 @@ public class MyUtil {
 	// 1~10까지의 총합을 구하는 동작
 	public int hap() {
 		int sum = 0;
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 100; i++) {
 			sum+=i;
 		}
 		return sum;
-		//삐삐야 이것 좀 해봐 ^^
+		
 	}
 }
